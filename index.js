c86d8ab89797c7c28930466bd8f8fefb71a86674
Kelly Park <kelly@Kellys-MacBook-Pro.local>
2019-07-25 16:47:18 +1000

    kelly: merge some groups

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var data = {
         'Begin retiring the MyCRM AngularJS code in favour of React',
         'Complete the migration of the MyCRM web app from AngularJS to React'
     ],
-    'API surface consolidation': [
+    'API surface consolidation & Reporting': [
         'Merge the MyCRM database and API repositories',
         'Bring the MyCRM database under version control using DbUp',
         'Begin migrating to .NET Core',
@@ -23,38 +23,32 @@ var data = {
         'Begin publishing domain events using the mediator pattern',
         'Begin publishing DTO updates to the shared data lake',
         'Complete the migration from .NET Framework to .NET Core',
+        'Introduce trunk-based development',
+        'Introduce feature toggles',
+        'Introduce continuous deployment to production',
         'Retire MS SQL Server in favour of Postgres with Marten as a NoSQL abstraction layer',
         'Introduce structured logging',
         'Introduce application health checks',
         'Introduce production monitoring and alerting',
         'Begin running synthetic transactions against the production environment'
     ],
-    'Reporting': [
-        ,
-    ],
-    'Automated test suite': [
+    'Automated test suite, Infrastructure as code & Hosting Migration': [
         'Begin running end-to-end tests nightly on a newly-provisioned environment',
         'Begin introducing test-driven development (TDD)',
         'Progressively reduce end-to-end test execution time',
         'Begin running end-to-end tests on every build on a newly-provisioned environment',
         'Introduce consumer-driven contract (CDC) tests',
-    ],
-    'Infrastructure as code & Hosting Migration': [
         'Introduce Terraform to provision application-level infrastructure (databases, S3 buckets etc.)',
         'Build Docker images using Docker Compose (and retire Packer and AMIs)',
         'Deploy the MyCRM API to the Kubernetes cluster',
     ],
+
     'Performance and scalability': [
         'Introduce performance SLAs as code',
         'Begin publishing structured business events (separate to domain events) to Dynamo or Kafka',
         'Introduce event choreography based on structured business events',
         'Introduce websockets to enable client-side applications to respond to business events',
     ],
-    'Continous deployment': [
-        'Introduce trunk-based development',
-        'Introduce feature toggles',
-        'Introduce continuous deployment to production',
-    ]
 };
 
 var nodeIndex = 1;
